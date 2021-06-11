ae684cd414a72bc714f82bd14bb462a8cfbac698
rabram1 <romanabram@web.de>
2021-06-11 15:57:40 +0200

    Add API function and outsourced api key with .env

---
+++ src/server/fmpSearch.ts
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { API_KEY } = process.env;
+
+type ErrorResult = {
+  message: string;
+};
+
+export type Query = {
+  stock: string;
+  time: string;
+};
+
+async function fetchSingleStockbyTime(
+  stock: string,
+  time: string
+): Promise<[]> {
+  const url = `https://financialmodelingprep.com/api/v3/historical-chart/${time}/${stock}?apikey=${API_KEY}`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    const errorResult: ErrorResult = await response.json();
+    console.log(errorResult);
+    throw {
+      message: errorResult.message,
+    };
+  }
+  const result = await response.json();
+  return result;
+}
+export default fetchSingleStockbyTime;
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import fetchSingleStockbyTime from './fmpSearch';
 
 const router = express.Router();
 
-router.get('/crypto', (_req, res) => {
-  res.status(404).send('Hello World');
+router.get('/singleStock', async (_req, res, next) => {
+  try {
+    const { query } = _req;
+    if (typeof query.stock !== 'string' || typeof query.time !== 'string') {
+      res.status(404).send('Hello World');
+      return;
+    }
+    const repos = await fetchSingleStockbyTime(query.stock, query.time);
+    res.status(200).json(repos);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
