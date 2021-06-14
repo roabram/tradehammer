3ba1e85a63fbe1f038b0659656bdd19d72553f4a
rabram1 <romanabram@web.de>
2021-06-14 15:41:43 +0200

    Add testing

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-type ButtonProps = {
-  children: string;
-};
-
-function Button(ButtonProps): JSX.Element {
-  return (
-    <button className={styles.button} onClick={() => alert('Good Job')}>
-      Hol dir den Alert
-    </button>
-  );
+function Button(
+  props: React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
+): JSX.Element {
+  return <button className={styles.button} {...props} />;
 }
 
 export default Button;
