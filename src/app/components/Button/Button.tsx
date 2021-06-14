976b85e69114e2ce6b52c66a565da85275d796a5
rabram1 <romanabram@web.de>
2021-06-14 15:29:21 +0200

    Add testing dependencies

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-function Button(): JSX.Element {
+type ButtonProps = {
+  children: string;
+};
+
+function Button(ButtonProps): JSX.Element {
   return (
     <button className={styles.button} onClick={() => alert('Good Job')}>
       Hol dir den Alert
