a282504bbbc2c4a81b080e8bde5eb8dce089859c
rabram1 <romanabram@web.de>
2021-06-15 10:38:00 +0200

    Addd Button for tests with jest

--- src/app/components/Button/Button.test.tsx
+++ src/app/components/Button/Button.test.tsx
@@ -3,6 +3,6 @@ import { render, screen } from '@testing-library/react';
 import Button from './Button';
 
 test('renders Start Button', () => {
-  render(<Button>Irgendetwas</Button>);
+  render(<Button>Hallo</Button>);
   expect(screen.getByRole('button').innerHTML).toBe('Hallo');
 });
