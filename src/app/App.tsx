e409cbadb57fedff6890a8a4d965b4b2921fb644
rabram1 <romanabram@web.de>
2021-06-21 11:43:38 +0200

    Add Home-page route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Button from './components/Button/Button';
-import CandleStickChart from './components/CandleStickChart/CandleStickChart';
+import Home from './pages/home/home';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/help">
-          <p>Help</p>
-        </Route>
+        <Route exact path="/Home" component={Home}></Route>
         <Route exact path="/">
           <p>Nein</p>
-          <Button onClick={() => alert('hi')}>Nicht Hallo</Button>
-          <CandleStickChart />
+          {/* <Button onClick={() => alert('hi')}>Nicht Hallo</Button>
+          <CandleStickChart /> */}
         </Route>
       </Switch>
     </BrowserRouter>
