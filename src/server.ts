ade14e018435bc5f7edb0b70ea5fe7fa2a202ced
rabram1 <romanabram@web.de>
2021-06-22 18:00:06 +0200

    ADd mongoDB to server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,9 @@ dotenv.config();
 import express from 'express';
 import router from './server/routes';
 import path from 'path';
-
+const { PORT = 6000 } = process.env;
 const app = express();
-const { PORT = 3333 } = process.env;
-
-const start = async () => {
-  if (process.env.MONGO_URL === undefined) {
-    throw new Error('Missing env MONGO_URL');
-  }
-
-  await connectDatabase(process.env.MONGO_URL);
-};
+app.use(express.json());
 
 app.use('/api', router);
 
@@ -25,13 +17,20 @@ app.use('/storybook', express.static('dist/storybook'));
 // Serve app production bundle
 app.use(express.static('dist/app'));
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
-
 // Handle client routing, return all requests to the app
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'app/index.html'));
 });
+
+const start = async () => {
+  if (process.env.MONGO_URL === undefined) {
+    throw new Error('Missing env MONGO_URL');
+  }
+  await connectDatabase(process.env.MONGO_URL);
+  console.log('Database connected');
+  app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+  });
+};
 
 start();
