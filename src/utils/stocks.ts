094d429f54f897400dee591d0bf986a7e2159d22
rabram1 <romanabram@web.de>
2021-06-22 18:01:14 +0200

    Add function to add and read stocks out of mongodb

---
+++ src/utils/stocks.ts
@@ -0,0 +1,10 @@
+import { StocksType } from '../types';
+import { getStocksCollection } from './database';
+
+export const saveStock = async (symbol: StocksType): Promise<void> => {
+  await getStocksCollection().insertOne({ ...symbol });
+};
+
+export const readStockData = async (): Promise<StocksType[]> => {
+  return await getStocksCollection().find().toArray();
+};
