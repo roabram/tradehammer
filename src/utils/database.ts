9cbd0d16dc35913e599c722767d3c9096eac7884
rabram1 <romanabram@web.de>
2021-06-22 18:00:47 +0200

    Add database function

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,3 +1,24 @@
+import { Collection, MongoClient } from 'mongodb';
+import { StocksType } from '../types';
+
+let client: MongoClient;
+
 export const connectDatabase = async (url: string): Promise<void> => {
-  console.log(url);
+  client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+};
+
+export const disconnectDatabase = async (): Promise<void> => {
+  return client.close();
+};
+
+export const getCollection = <T>(symbol: string): Collection<T> => {
+  return client.db().collection<T>(symbol);
+};
+
+export const getStocksCollection = (): Collection<StocksType> => {
+  return getCollection<StocksType>('stocks');
 };
