4820a13f99fb909c3e5986b8985ffa0ad1fbcbb0
rabram1 <romanabram@web.de>
2021-06-28 11:02:37 +0200

    Add stock delete function

--- src/utils/stocks.ts
+++ src/utils/stocks.ts
@@ -8,3 +8,7 @@ export const saveStock = async (symbol: StocksType): Promise<void> => {
 export const readStockData = async (): Promise<StocksType[]> => {
   return await getStocksCollection().find().toArray();
 };
+
+export const deleteStockData = async (symbol: StocksType): Promise<void> => {
+  await getStocksCollection().deleteOne(symbol);
+};
