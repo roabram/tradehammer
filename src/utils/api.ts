7d00e00fe5bf5f69f77bb3826e0ccb6b32b7298a
rabram1 <romanabram@web.de>
2021-06-28 11:02:56 +0200

    Add delete StockFunction

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-import { StocksType } from '../types';
+import { StocksType, UserType } from '../types';
 
 export async function postSearchResult(symbol: string): Promise<StocksType> {
   const response = await fetch('/api/stocks', {
@@ -13,5 +13,39 @@ export async function postSearchResult(symbol: string): Promise<StocksType> {
     throw errorMessage;
   }
   const result: StocksType = await response.json();
+  return result;
+}
+
+export async function deleteSingleStock(symbol: string): Promise<StocksType> {
+  const response = await fetch('/api/stocks', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ symbol: symbol }),
+  });
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw errorMessage;
+  }
+  const result: StocksType = await response.json();
+  return result;
+}
+
+export async function postLoginUser(
+  user: Partial<UserType>
+): Promise<UserType> {
+  const response = await fetch('/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw errorMessage;
+  }
+  const result: UserType = await response.json();
   return result;
 }
