4b147e409cb37f1bdbff70834471de362b9a2649
rabram1 <romanabram@web.de>
2021-06-28 10:57:35 +0200

    Add dashboard page route to App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
 import Home from './pages/home/Home';
-import Login from './pages/Login/Login';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/Home" component={Home}></Route>
-        <Route exact path="/Login" component={Login}></Route>
+        <Route exact path="/home" component={Home}></Route>
+        <Route exact path="/dashboard" component={Dashboard}></Route>
       </Switch>
     </BrowserRouter>
   );
