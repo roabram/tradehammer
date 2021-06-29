74f55673278700d934f3310cb13f39f333ed7390
rabram1 <romanabram@web.de>
2021-06-29 16:18:42 +0200

    Replace page heading with App heading

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import Home from './pages/home/Home';
 import './App.modules.css';
+import Heading from './components/Heading/Heading';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
+      <Heading />
       <Switch>
         <Route exact path="/home" component={Home}></Route>
         <Route exact path="/dashboard" component={Dashboard}></Route>
